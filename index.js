9437d0b22e915560439da4d5ea7d8325a2923510
Nesmeyanov Kirill <k.nesmeyanov@rambler-co.ru>
2017-04-18 17:45:03 +0300

    Improve index

---
+++ index.js
@@ -0,0 +1,5 @@
+export Container from "./src/Di/Container";
+
+export Reader from "./src/Annotation/Reader";
+export Target from "./src/Annotation/Target";
+export Annotation from "./src/Annotation/Annotation";
--- src/index.js
+++
@@ -1,3 +0,0 @@
-export Container from "./Di/Container";
-export Reader from "./Annotation/Reader";
-export Target from "./Annotation/Target";
